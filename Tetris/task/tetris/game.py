2b55876efef033d9b3c6be52f7cbbe88f1a8a8d7
fvn70 <fvn_reg@mail.ru>
2022-08-14 13:29:13 +0700

    Stage 2/4: I Like to Move It

--- Tetris/task/tetris/game.py
+++ Tetris/task/tetris/game.py
@@ -1,27 +1,94 @@
-O = [[5, 6, 9, 10]]
-I = [[1, 5, 9, 13], [4, 5, 6, 7]]
-S = [[6, 5, 9, 8], [5, 9, 10, 14]]
-Z = [[4, 5, 9, 10], [2, 5, 6, 9]]
-L = [[1, 5, 9, 10], [2, 4, 5, 6], [1, 2, 6, 10], [4, 5, 6, 8]]
-J = [[2, 6, 9, 10], [4, 5, 6, 10], [1, 2, 5, 9], [0, 4, 5, 6]]
-T = [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]]
+class Field:
+    items = []
+
+    def __init__(self, cols, rows):
+        self.n = rows
+        self.m = cols
+
+    def add_item(self, p):
+        self.items = []
+        self.items.append(p)
+
+    def down(self, p):
+        p.down(self.m)
+
+    def left(self, p):
+        p.left(self.m)
+
+    def right(self, p):
+        p.right(self.m)
+
+    def rotate(self, p):
+        p.rotate()
+
+    def draw(self):
+        lst = []
+        for p in self.items:
+            lst += p.shapes[p.idx]
+        k = 0
+        print()
+        for r in range(self.n):
+            s = ''
+            for c in range(self.m):
+                s += '0 ' if k in lst else '- '
+                k += 1
+            print(s.strip())
+
+class Figure:
+
+    def __init__(self, name, shapes):
+        self.name = name
+        self.shapes = shapes
+        self.idx = 0
+        self.turn = 0
+        self.turns = len(shapes)
+
+    def down(self, m):
+        self.shapes = [[p[j] + m for j in range(4)] for p in self.shapes]
+
+    def left(self, m):
+        self.shapes = [[p[j] - 1 if p[j] % m > 0 else p[j] + m - 1 for j in range(4)] for p in self.shapes]
+
+    def right(self, m):
+        self.shapes = [[p[j] + 1 if p[j] % m < m - 1 else p[j] - m + 1 for j in range(4)] for p in self.shapes]
+
+    def rotate(self):
+        self.turn += 1
+        self.idx = self.turn % self.turns
+
+
+O = [[4, 14, 15, 5]]
+I = [[4, 14, 24, 34], [3, 4, 5, 6]]
+S = [[5, 4, 14, 13], [4, 14, 15, 25]]
+Z = [[4, 5, 15, 16], [5, 15, 14, 24]]
+L = [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]]
+J = [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]]
+T = [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]]
 
 dic = {'O': O, 'I': I, 'S': S, 'Z': Z, 'L': L, 'J': J, 'T': T, }
 
-def draw(letter):
-    pieces = dic[letter]
-    r = len(pieces)
-    pieces = (4 // r + 1) * pieces
-    p = []
-    for i in range(6):
-        for r in range(4):
-            for c in range(4):
-                k = r * 4 + c
-                print('0 ' if k in p else '- ', end='')
-            print()
-        print()
-        if i < 5:
-            p = pieces[i]
 
+fig = input()
+m, n = input().split()
+# m, n = '10', '20'
+g = Field(int(m), int(n))
+g.draw()
+f = Figure(fig, dic[fig].copy())
+g.add_item(f)
+g.draw()
 
-draw(input())
+while True:
+    cmd = input()
+    if cmd == 'exit':
+        break
+    elif cmd == 'down':
+        pass
+    elif cmd == 'left':
+        g.left(f)
+    elif cmd == 'right':
+        g.right(f)
+    elif cmd == 'rotate':
+        g.rotate(f)
+
+    g.down(f)
+    g.draw()
